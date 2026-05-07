23304352cf5494d9c3b43a7bb18976f2e8a42708
srescs <srescsolutions@gmail.com>
2026-05-07 12:30:39 +0000

    Add git ignore

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/workspaces/sres-solutions/app/layout\":[\"static/media/13971731025ec697-s.p.woff2\",\"static/media/8a1d8947e5852e30-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
